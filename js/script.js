2425804151e36384783a79bf99a4493093719b94
Ken Karlson <kenkarlson@mail.ru>
2021-11-26 07:59:44 +0300

    create: lesson02

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 "use strict"
-let title,
-  screens,
-  screenPrice,
-  rollback,
-  fullPrice,
-  adaptive;
+let title = 'I learning JS',
+  screens = 'Простые, Сложные, Интерактивные',
+  screenPrice = 99,
+  rollback = 2,
+  fullPrice = 150000,
+  adaptive = true;
 
-console.log('Hello world');
-
-alert('Hello world');
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+screens = screens.toLowerCase();
+console.log(screens.split(','));
+console.log((fullPrice * (rollback / 100)));
