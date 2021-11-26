8a4f82f1c47b26aa9c22cf638b7ef5ed1e20a6f8
Ken Karlson <kenkarlson@mail.ru>
2021-11-26 08:23:50 +0300

    create: difficult task 01

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 "use strict"
-let title = 'I learning JS',
-  screens = 'Простые, Сложные, Интерактивные',
-  screenPrice = 99,
-  rollback = 2,
-  fullPrice = 150000,
-  adaptive = true;
-
-console.log(title, fullPrice, adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
-console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
-screens = screens.toLowerCase();
-console.log(screens.split(','));
-console.log((fullPrice * (rollback / 100)));
+let num = 266219;
+let arr = ('' + num).split('');
+console.log(arr);
+let temp = 1;
+for (let i = 0; i < arr.length; i++) {
+  temp = temp * arr[i];
+}
+console.log(temp);
+console.log(String(temp ** 3).slice(0, 2));
