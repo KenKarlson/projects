f3ccc16d6da4528422902c55477467ea6bcb57ac
Ken Karlson <kenkarlson@mail.ru>
2021-11-29 19:11:39 +0300

    create: lesson03

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-"use strict"
-let num = 266219;
-let arr = ('' + num).split('');
-console.log(arr);
-let temp = 1;
-for (let i = 0; i < arr.length; i++) {
-  temp = temp * arr[i];
+'use strict';
+
+let title = prompt('Как называется ваш проект ? ', '');
+let screen = prompt('Какие типы экранов нужно разработать? ', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000, 15000, 30000');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service01 = prompt('Какой дополнительный тип услуги нужен?', '');
+let servicePrice01 = +prompt('Сколько будет стоить данная работа?');
+
+let service02 = prompt('Какой дополнительный тип услуги нужен?', '');
+let servicePrice02 = +prompt('Сколько будет стоить данная работа?');
+
+let managerInterest = 0.15;
+
+let fullPrice = screenPrice + servicePrice01 + servicePrice02;
+console.log('Стоимость: ' + fullPrice);
+let managerPrice = fullPrice * managerInterest;
+console.log('ЗП Манагера: ' + managerPrice);
+let servicePercentPrice = fullPrice - (fullPrice * managerInterest);
+console.log('Доход: ' + servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  fullPrice = fullPrice - (fullPrice * 0.1);
+  console.log('Даем скидку в 10 %');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  fullPrice = fullPrice - (fullPrice * 0.05);
+  console.log('Даем скидку в 5 %');
+} else if (fullPrice < 15000 && fullPrice > 0) {
+  console.log('Скидка не предусмотрена');
+} else {
+  console.log('Что то пошло не так');
 }
-console.log(temp);
-console.log(String(temp ** 3).slice(0, 2));
