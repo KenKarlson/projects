bd6ecb16f1a1747e65339f06b121630c5757d2e4
Ken Karlson <kenkarlson@mail.ru>
2021-11-30 00:48:12 +0300

    fix: add to object

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 
 let language = prompt('Введите язык', 'ru en');
 
-
+const dayOfTheWeek = {
+  'Понедельник': 'Monday',
+  'Вторник': 'Tuesday',
+  'Среда': 'Wednesday',
+  'Четверг': 'Thursday',
+  'Пятница': 'Friday',
+  'Субота': 'Saturday',
+  'Воскресенье': 'Sunday'
+};
 
 let daysOfTheWeekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Субота', 'Воскресенье'];
 let daysOfTheWeekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -17,13 +25,21 @@ function getRuDay() {
 function getEnDay() {
   console.log(JSON.stringify(daysOfTheWeekEn));
 }
-
 if (language) {
-
+  let out = '';
   if (temp == 'ru') {
     console.log(JSON.stringify(daysOfTheWeekRu));
+
+    for (let key in dayOfTheWeek) {
+      out += key + ', ';
+    }
+    console.log(out);
   } else if (temp == 'en') {
     console.log(JSON.stringify(daysOfTheWeekEn));
+    for (let key in dayOfTheWeek) {
+      out += dayOfTheWeek[key] + ', ';
+    }
+    console.log(out);
   } else {
     console.log('Непонятненько');
   }
@@ -54,5 +70,10 @@ function testPerson(person) {
 
   console.log(person);
 }
-
 testPerson(namePerson);
+
+function getLangDay(temp) {
+
+
+
+}
