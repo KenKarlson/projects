1905642837ea5201fb314d1cc8078baef3f49761
Ken Karlson <kenkarlson@mail.ru>
2021-11-29 20:32:19 +0300

    create: difficult task 03

--- js/script.js
+++ js/script.js
@@ -1,10 +1,58 @@
-"use strict"
-let num = 266219;
-let arr = ('' + num).split('');
-console.log(arr);
-let temp = 1;
-for (let i = 0; i < arr.length; i++) {
-  temp = temp * arr[i];
+'use strict';
+
+let language = prompt('Введите язык', 'ru en');
+
+
+
+let daysOfTheWeekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Субота', 'Воскресенье'];
+let daysOfTheWeekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
+let temp = language.toLowerCase();
+
+function getRuDay() {
+  console.log(JSON.stringify(daysOfTheWeekRu));
 }
-console.log(temp);
-console.log(String(temp ** 3).slice(0, 2));
+
+function getEnDay() {
+  console.log(JSON.stringify(daysOfTheWeekEn));
+}
+
+if (language) {
+
+  if (temp == 'ru') {
+    console.log(JSON.stringify(daysOfTheWeekRu));
+  } else if (temp == 'en') {
+    console.log(JSON.stringify(daysOfTheWeekEn));
+  } else {
+    console.log('Непонятненько');
+  }
+} else {
+  console.log('Непонятненько');
+}
+
+function newSwitch() {
+  switch (temp) {
+    case 'ru':
+      getRuDay();
+      break;
+    case 'en':
+      getEnDay();
+      break;
+    default:
+      console.log('Непонятненько');
+      break;
+  }
+}
+newSwitch();
+
+let namePerson = prompt('введите имя', 'Артем, Александр, другое');
+
+function testPerson(person) {
+  person = (namePerson == 'Артем') ? 'Директор' :
+    (namePerson == 'Александр') ? 'Преподаватель' : 'Студент';
+
+  console.log(person);
+}
+
+testPerson(namePerson);
