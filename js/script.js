bc54d37df6b72e03101bf4c94fe567afedd40142
Ken Karlson <kenkarlson@mail.ru>
2021-12-08 01:07:14 +0300

    create: elements for js

--- js/script.js
+++ js/script.js
@@ -1,127 +1,27 @@
-'use strict';
+'use ctrict';
 
-const appData = {
-  title: '',
-  screens: [],
-  screenPrice: 0,
-  adaptive: true,
-  rollback: 10,
-  fullPrice: 0,
-  servicePercentPrice: 0,
-  allServicePrices: 0,
-  services: {},
+const title = document.getElementsByTagName('h1')[0].innerHTML;
+const handlerBtnStart = document.getElementsByClassName('handler_btn')[0];
+const handlerBtnReset = document.getElementsByClassName('handler_btn')[1];
+const screenBtn = document.querySelector('.screen-btn');
+const otherItemsPercent = document.querySelectorAll('.percent');
+const otherItemsNumber = document.querySelectorAll('.number');
 
-  start: function () {
-    appData.asking();
-    appData.getAllServicePrices();
-    appData.getFullPrice();
-    appData.getServicePercentPrices();
-    appData.getTitle(appData.title);
+const rangeValueSpan = document.querySelector('.rollback').querySelector('.range-value');
 
-    appData.logger();
-  },
+const totalInput = document.getElementsByClassName('total-input');
 
-  asking: function () {
-    do {
-      appData.title = prompt('Как называется ваш проект ?');
-    }
-    while (appData.isString(appData.title));
+let screenSelect = document.querySelectorAll('.screen');
 
+console.log(title);
+console.log(handlerBtnStart);
+console.log(handlerBtnReset);
+console.log(screenBtn);
+console.log(otherItemsPercent);
+console.log(otherItemsNumber);
 
-    for (let i = 0; i < 2; i++) {
-      let name;
-      do {
-        name = prompt('Какие типы экранов нужно разработать? ');
-      }
-      while (appData.isString(name));
-      let price = 0;
-      do {
-        price = prompt('Сколько будет стоить данная работа?');
-      }
-      while (!appData.isNumber(price));
-      appData.screens.push({
-        id: i,
-        name: name + i,
-        price: price
-      });
+console.log(totalInput);
 
-    }
-    for (let i = 0; i < 2; i++) {
-      let name;
-      let price = 0;
+console.log(screenSelect);
 
-      do {
-        name = prompt('Какой дополнительный тип услуги нужен?');
-      }
-      while (appData.isString(name));
-
-      do {
-        price = prompt('Сколько будет стоить данная работа?');
-      }
-      while (!appData.isNumber(price));
-      appData.services[name + i] = +price;
-    }
-    appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-  },
-  getAllServicePrices: function () {
-
-
-
-    for (let key in appData.services) {
-      appData.allServicePrices += appData.services[key];
-    }
-  },
-  isNumber: function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  },
-  isString: function (str) {
-    return !isNaN(parseFloat(str));
-  },
-  getTitle: function (elem) {
-    let temp;
-    if (elem) {
-      temp = elem.trim().charAt(0).toUpperCase() + elem.trim().substr(1).toLowerCase();
-    } else {
-      temp = 'Новый проект';
-    }
-    appData.title = temp;
-  },
-  getRollbackMessage: function (price) {
-    let temp;
-    if (price >= 30000) {
-      temp = 'Даем скидку в 10 %';
-    } else if (price >= 15000 && price < 30000) {
-      temp = 'Даем скидку в 5 %';
-    } else if (price < 15000 && price > 0) {
-      temp = 'Скидка не предусмотрена';
-    } else {
-      temp = 'Что то пошло не так';
-    }
-    return temp;
-  },
-  getServicePercentPrices: function () {
-    appData.servicePercentPrice = Math.ceil(appData.fullPrice - (appData.fullPrice * (appData.rollback / 100)));
-  },
-  getAllScreens: function (elem) {
-    let temp = [];
-    if (elem) {
-      temp = elem.split(' ');
-    }
-    return temp;
-  },
-  getFullPrice: function () {
-    appData.screenPrice = appData.screens.reduce((s, i) => s = s + Number.parseInt(i.price), 0);
-
-    appData.fullPrice = Number.parseInt(appData.screenPrice) + Number.parseInt(appData.allServicePrices);
-  },
-  logger: function () {
-    console.log(appData.fullPrice);
-    console.log(appData.servicePercentPrice);
-    console.log(appData.screens);
-    console.log(appData.services);
-    console.log('Finish');
-  }
-};
-
-
-appData.start();
+console.log(rangeValueSpan);
