2bacbdf1779e3abc9e75340d144328b70aa4cc2f
Ken Karlson <kenkarlson@mail.ru>
2021-12-10 21:44:04 +0300

    Revert "fix: all"
    This reverts commit f5d86157b2b7d58473868739d0f8628dc3f053b8.

---
+++ js/test.js
@@ -0,0 +1,34 @@
+// let a = 'abc';
+// let b = 12345;
+// let c = prompt('ввод данных для проверки');
+
+
+// const isString = function (n) {
+//   let temp = [];
+//   if (n > 1) {
+//     temp = n.split('');
+//     console.log(temp[0]);
+//   }
+// };
+// isString(a);
+
+// const foo = function () {
+//   let arr = [{
+//       number: 3
+//     },
+//     {
+//       number: 10
+//     },
+//     {
+//       number: 13
+//     }
+//   ];
+
+//   let initialValue = 0;
+
+//   let result = arr.reduce(function callback(accumulator, currentValue) {
+//     return accumulator = accumulator + currentValue.number;
+//   }, 0);
+
+//   console.log(`result равен: ${result}`);
+// };
